29313708c03a315331739e4549718bac709fc837
MohammedKhalidJ <mohammedkhalid11@gmail.com>
2019-09-27 16:51:21 -0500

    Education page
    Card Changes

--- src/components/EducationView.jsx
+++ src/components/EducationView.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 
-const EducationView = () => <div class="row">{cards()}</div>;
+const EducationView = () => (
+  <div class="container" style={{ padding: "5%" }}>
+    <div class="row">
+      {cards("Secondary", "2007 - 2008")}
+      {cards("Higher Secondary", "2009 - 2010")}
+      {cards("University", "2010 - 2014")}
+    </div>
+  </div>
+);
 
-const cards = () => (
+const cards = (title, subtitle) => (
   <div class="col-md-4">
     <div class="card" style={{ width: "400" }}>
-      <img
-        class="card-img-top"
-        src="img_avatar1.png"
-        alt="Card"
-        style={{ width: "100%" }}
-      />
       <div class="card-body">
-        <h4 class="card-title">John Doe</h4>
-        <p class="card-text">
-          Some example text some example text. John Doe is an architect and
-          engineer
-        </p>
+        <h4 class="card-title">{title}</h4>
+        <p class="card-text">{subtitle}</p>
         <button class="btn btn-primary stretched-link">See Profile</button>
       </div>
     </div>
