fa44dbb7693973e4ffdf31b13d83fc35031d8b99
Mohammed Khalid <mohammedkhalid11@gmail.com>
2019-12-18 14:55:55 -0600

    Updates

---
+++ precache-manifest.971fdf5d22f8f3c693b7153e34c0f3fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909e091d31f58c01f7f246d27b21d9f7",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "daee9fffa3673760d494",
+    "url": "/MyResume/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "b6be3466e9940b9f994a",
+    "url": "/MyResume/static/css/main.16e6fd4f.chunk.css"
+  },
+  {
+    "revision": "daee9fffa3673760d494",
+    "url": "/MyResume/static/js/2.834b3443.chunk.js"
+  },
+  {
+    "revision": "b6be3466e9940b9f994a",
+    "url": "/MyResume/static/js/main.2bff4d55.chunk.js"
+  },
+  {
+    "revision": "c777bb308bc8e9d8e4f8",
+    "url": "/MyResume/static/js/runtime~main.17fc0005.js"
+  },
+  {
+    "revision": "55c2920f37bbfd60473b8d0deea3861b",
+    "url": "/MyResume/static/media/index-bng.55c2920f.jpg"
+  }
+]);
