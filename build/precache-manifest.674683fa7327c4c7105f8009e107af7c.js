099885d8448ae710d836a450eb851a13d5d8a778
Mohammed Khalid <mohammedkhalid11@gmail.com>
2019-12-18 17:00:10 -0600

    Build changes

---
+++ build/precache-manifest.674683fa7327c4c7105f8009e107af7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad1cc5222b452729cec38c9abe3f772",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "daee9fffa3673760d494",
+    "url": "/MyResume/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "8e6238f9d44931775587",
+    "url": "/MyResume/static/css/main.16e6fd4f.chunk.css"
+  },
+  {
+    "revision": "daee9fffa3673760d494",
+    "url": "/MyResume/static/js/2.834b3443.chunk.js"
+  },
+  {
+    "revision": "8e6238f9d44931775587",
+    "url": "/MyResume/static/js/main.7b225345.chunk.js"
+  },
+  {
+    "revision": "c777bb308bc8e9d8e4f8",
+    "url": "/MyResume/static/js/runtime~main.17fc0005.js"
+  },
+  {
+    "revision": "55c2920f37bbfd60473b8d0deea3861b",
+    "url": "/MyResume/static/media/index-bng.55c2920f.jpg"
+  }
+]);
